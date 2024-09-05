df30f4ea5b13c7fc3ee216a96f4b9ef12279fe46
t-ueno-ruby <t-ueno@ruby-dev.jp>
2024-09-05 15:46:09 +0900

    v-on

--- how-to-use-vue3/js/main.js
+++ how-to-use-vue3/js/main.js
@@ -1,7 +1,14 @@
 const app = Vue.createApp({
   // ここにオプションを追加していく
   data: () => ({
-
-  })
+    now: '-'
+  }),
+  methods: {
+    onClick: function () {
+      // console.log('onclick');
+      // console.log(this);
+      this.now = new Date().toLocaleString()
+    }
+  }
 })
 app.mount('#app')
