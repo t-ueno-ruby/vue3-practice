08d7754aa9aa5623212c13fd409812dcf4254bca
t-ueno-ruby <t-ueno@ruby-dev.jp>
2024-09-05 15:31:56 +0900

    初期化

--- how-to-use-vue3/js/main.js
+++ how-to-use-vue3/js/main.js
@@ -1,12 +1,7 @@
 const app = Vue.createApp({
   // ここにオプションを追加していく
   data: () => ({
-    colors: ['red', 'green', 'blue'],
-    user: {
-      firstName: 'Taro',
-      lastName: 'Yamada',
-      age: 23,
-    }
+
   })
 })
 app.mount('#app')
