cd70cea7209e3fdcd88102355d094fe7f27932f9
t-ueno-ruby <t-ueno@ruby-dev.jp>
2024-09-05 14:59:49 +0900

    データバインディング

--- how-to-use-vue3/js/main.js
+++ how-to-use-vue3/js/main.js
@@ -1,7 +1,14 @@
 const app = Vue.createApp({
   // ここにオプションを追加していく
   data: () => ({
-    message: 'Hello Vue.js'
+    message: 'Hello Vue.js',
+    count: 99,
+    user: {
+      lastName: 'Nakamura',
+      firstName: 'Yota',
+      prefecture: 'Oita'
+    },
+    colors: ['Red', 'Green', 'Blue']
   })
 })
 app.mount('#app')
