c4954f6d94b30afd2f293b645645c3498887fbf6
t-ueno-ruby <t-ueno@ruby-dev.jp>
2024-09-05 15:48:49 +0900

    初期化

--- how-to-use-vue3/js/main.js
+++ how-to-use-vue3/js/main.js
@@ -1,14 +1,6 @@
 const app = Vue.createApp({
   // ここにオプションを追加していく
   data: () => ({
-    now: '-'
   }),
-  methods: {
-    onClick: function () {
-      // console.log('onclick');
-      // console.log(this);
-      this.now = new Date().toLocaleString()
-    }
-  }
 })
 app.mount('#app')
