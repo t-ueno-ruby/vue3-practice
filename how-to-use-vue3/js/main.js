0160c14313227652c130dd80748c44920ed59642
t-ueno-ruby <t-ueno@ruby-dev.jp>
2024-09-06 14:22:02 +0900

    初期化

--- how-to-use-vue3/js/main.js
+++ how-to-use-vue3/js/main.js
@@ -1,24 +1,6 @@
 const app = Vue.createApp({
   // ここにオプションを追加していく
-  data: () => ({
-    newItem: '',
-    todos: [],
-  }),
-  methods: {
-    addItem: function (event) {
-      if (this.newItem === '') return;
-      let todo = {
-        item: this.newItem,
-        isDone: false,
-      };
-      this.todos.push(todo);
-
-      this.newItem = '';
-    },
-    deleteItem: function (index) {
-      this.todos.splice(index, 1);
-    },
-  },
+  data: () => ({}),
 });
 
 app.mount('#app');
