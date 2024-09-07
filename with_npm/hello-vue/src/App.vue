1adc4ffd5fe2424b01bfc26839af728ad53aa06e
t-ueno-ruby <t-ueno@ruby-dev.jp>
2024-09-07 21:13:12 +0900

    初期化

--- with_npm/hello-vue/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-const imgAttributes = ref({ src: '/images/logo.svg', alt: 'Vueのロゴ', width: 75, height: 75 })
-</script>
-<template>
-  <p><img v-bind="imgAttributes" /></p>
-  <p><img v-bind="imgAttributes" title="ロゴです!" /></p>
-  <p><img v-bind="imgAttributes" alt="ロゴです!" /></p>
-</template>
