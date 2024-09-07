7c6cd457cf8a79d3aaa56ee2a1601798b6bde890
t-ueno-ruby <t-ueno@ruby-dev.jp>
2024-09-07 21:07:44 +0900

    v-bind

--- with_npm/hello-vue/src/App.vue
+++ with_npm/hello-vue/src/App.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import { reactive, computed } from 'vue'
-// リアクティブなテンプレート変数をまとめて用意。
-const data = reactive({ PI: 3.14, radius: Math.round(Math.random() * 10) })
-// 円の面積の算出プロパティを用意。
-const area = computed((): number => {
-  return data.radius * data.radius * data.PI
-})
-// 半径のテンプレート変数に新しい乱数を1秒ごとに格納。
-setInterval((): void => {
-  data.radius = Math.round(Math.random() * 10)
-}, 1000)
+import { ref } from 'vue'
+const url = ref('https://vuejs.org/')
 </script>
-
 <template>
-  <p>半径{{ data.radius }}の円の面積を円周率{{ data.PI }}で計算すると、{{ area }}</p>
+  <p><a v-bind:href="url" target="_blank">Vue.jsのサイト</a></p>
+  <p><a :href="url" target="_blank">Vue.jsのサイト(省略系)</a></p>
+  <!-- v-bind -->
+  <p><a v-bind:href="url + 'guide/introduction.html'" target="_blank">Vue.jsガイドのページ</a></p>
+  <p><a :href="url + 'guide/introduction.html'" target="_blank">Vue.jsガイドのページ</a></p>
 </template>
